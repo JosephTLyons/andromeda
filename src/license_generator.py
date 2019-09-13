5cc04fc14fc6f7a406c3aa27e6d05d1af426570c
Joseph Lyons <josephtlyons@gmail.com>
2019-09-13 04:40:09 -0400

    Fix up stats printing section

--- src/license_generator.py
+++ src/license_generator.py
@@ -66,15 +66,26 @@ class LicenseGenerator:
         print("File path: {}".format(filePath))
 
     def __printStatsToTerminal(self):
-        print("Requested serial number amount: ", end = '')
-        print(self.requestedAmount)
-        print("Total possible serial numbers given current inputs: ", end = '')
+        self.__printRequestedAmount()
+        self.__printTotalPossibleSerialNumbers()
+        self.__printPercentOfLicensePoolCovered()
 
+    def __printRequestedAmount(self):
+        print("Requested serial number amount: " + str(self.requestedAmount))
+
+    def __printTotalPossibleSerialNumbers(self):
+        print("Total possible serial numbers given current inputs: ", end = '')
         print(self.serialCharacteristics.numberOfCharacters, end = '')
         print("^", end = '')
         print(self.serialCharacteristics.len, end = '')
         print(" = ", end = '')
         print(self.serialCharacteristics.totalPossibleSerialNumbers)
-        print("The printed licenses cover ", end = '')
+
+    def __printPercentOfLicensePoolCovered(self):
+        print("License pool coverage: (", end = '')
+        print(str(self.requestedAmount) + " / ", end = '')
+        print("(" + str(self.serialCharacteristics.numberOfCharacters), end = '')
+        print("^", end = '')
+        print(str(self.serialCharacteristics.len) + ")) * 100 = ", end = '')
         print(((self.requestedAmount / self.serialCharacteristics.totalPossibleSerialNumbers) * 100), end = '')
-        print("% of the total license pool: ", end = '')
+        print("%")
