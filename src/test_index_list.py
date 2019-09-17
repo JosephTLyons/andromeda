b02f88ec060a4202738f0ebcca89028afc02246f
Joseph Lyons <josephtlyons@gmail.com>
2019-09-17 15:35:36 -0400

    Adding some unit tests for index_list

---
+++ src/test_index_list.py
@@ -0,0 +1,20 @@
+import pytest
+from src/index_list import IndexList
+
+class TestIndexList():
+    def test_base_rollovers(self):
+        length = 5
+        bases = [10, 26, 62, 94]
+
+        for base in bases:
+            index_list = IndexList(length, base)
+
+            for i in range(length):
+                index_list.increaseBy(base ** i)
+
+            new_list = []
+
+            for i in range(index_list.len()):
+                new_list.append(index_list.at(i))
+
+            assert [1, 1, 1, 1, 1] == new_list
