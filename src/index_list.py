dc41eb57438e05f63a2295f86db50cf18719491e
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-02 18:19:24 -0400

    Fix variable name

--- src/index_list.py
+++ src/index_list.py
@@ -1,44 +1,44 @@
 class IndexList:
     def __init__(self, length, base):
-        self.indexList = [0] * length
+        self.index_list = [0] * length
         self.base = base
         self.has_overflowed = False
 
     def __getitem__(self, index):
-        return self.indexList[index]
+        return self.index_list[index]
 
     def __len__(self):
-        return len(self.indexList)
+        return len(self.index_list)
 
     def increase_by(self, amount):
         if amount <= 0:
             raise ValueError("Must increment by a positive value")
 
-        for i in reversed(range(len(self.indexList))):
-            self.indexList[i] += amount % self.base
+        for i in reversed(range(len(self.index_list))):
+            self.index_list[i] += amount % self.base
 
-            if (i == 0) and (amount + self.indexList[i] >= self.base):
+            if (i == 0) and (amount + self.index_list[i] >= self.base):
                 self.has_overflowed = True
 
             amount //= self.base
 
-            if self.indexList[i] >= self.base:
+            if self.index_list[i] >= self.base:
                 self.__carry_over(i)
             elif amount <= 0:
                 return
 
     def __carry_over(self, index):
-        self.indexList[index] -= self.base
+        self.index_list[index] -= self.base
 
         if index > 0:
-            self.indexList[index - 1] += 1
+            self.index_list[index - 1] += 1
 
     def get_index_string(self):
         padding = len(str(self.base))
 
         index_string = ""
 
-        for index in self.indexList:
+        for index in self.index_list:
             index_string += (str(index).rjust(padding, '0') + " ")
 
         return index_string
